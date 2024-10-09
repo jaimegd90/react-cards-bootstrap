4376846b14687a24ec6328f3b42a9b294f738888
Jaime González Díaz <jaimegd90@gmail.com>
2024-10-09 09:48:48 +0200

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "https://jaimegd90.github.io/react-cards-bootstrap/",
+  base: "https://github.com/jaimegd90/react-cards-bootstrap/",
   plugins: [react()],
 })
